d426e2447fd9cf3c600f668da956cdf025845fee
Ivan Suftin <isuftin@usgs.gov>
2014-03-05 21:29:24 -0600

    Updated data access module to use derby for integration testing because this will probably be the embedded choice

--- twitter-data-access/src/test/java/gov/usgs/cida/twitter/data/dao/EventTypeDAOTest.java
+++ twitter-data-access/src/test/java/gov/usgs/cida/twitter/data/dao/EventTypeDAOTest.java
@@ -2,14 +2,28 @@ package gov.usgs.cida.twitter.data.dao;
 
 import gov.usgs.cida.twitter.data.model.EventType;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
+import liquibase.CatalogAndSchema;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.FileSystemResourceAccessor;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.experimental.categories.Category;
 
 /**
@@ -18,23 +32,59 @@ import org.junit.experimental.categories.Category;
  */
 @Category(IntegrationTest.class)
 public class EventTypeDAOTest {
-    
+
+    private static Connection conn;
     private static SqlSessionFactory sqlSessionFactory;
-    
+    private static Liquibase liquibase;
+
     public EventTypeDAOTest() {
     }
-    
+
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass() throws ClassNotFoundException, SQLException, DatabaseException, LiquibaseException, InstantiationException, IllegalAccessException {
+        String port = System.getProperty("db.twitter.integration-test.port");
+        String driver = System.getProperty("db.twitter.integration-test.driver");
+        String dbType = System.getProperty("db.twitter.integration-test.dbtype");
+        String schema = System.getProperty("db.twitter.integration-test.schema");
+        if (StringUtils.isBlank(port)) {
+            throw new NullPointerException("System property \"db.twitter.integration-test.port\" not found");
+        }
+        if (StringUtils.isBlank(driver)) {
+            throw new NullPointerException("System property \"db.twitter.integration-test.driver\" not found");
+        }
+        if (StringUtils.isBlank(dbType)) {
+            throw new NullPointerException("System property \"db.twitter.integration-test.dbType\" not found");
+        }
+        if (StringUtils.isBlank(schema)) {
+            throw new NullPointerException("System property \"db.twitter.integration-test.schema\" not found");
+        }
+        Class.forName(driver).newInstance();
+        conn = DriverManager.getConnection("jdbc:" + dbType + "://localhost:" + port + "/" + schema + ";create=true", "test", "test");
+
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(conn));
+        liquibase = new Liquibase("src/main/resources/liquibase/changelogs/create-table-parent-changeLog.xml", new FileSystemResourceAccessor(), database);
+
         try (InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml")) {
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream, "integration-test");
         } catch (Exception ex) {
             System.out.println("Error initializing SqlSessionFactoryBuilder: " + ex);
         }
     }
-    
+
     @AfterClass
-    public static void tearDownClass() {
+    public static void tearDownClass() throws SQLException {
+        conn.close();
+    }
+
+    @Before
+    public void beforeTest() throws DatabaseException, LiquibaseException {
+        liquibase.dropAll(new CatalogAndSchema("test", "twitter"));
+        liquibase.update("");
+    }
+
+    @After
+    public void afterTest() throws DatabaseException {
+        liquibase.dropAll(new CatalogAndSchema("test", "twitter"));
     }
 
     @Test
@@ -53,5 +103,5 @@ public class EventTypeDAOTest {
         EventType result = instance.getByEventTypeId(id);
         assertNotNull(result);
     }
-    
+
 }
